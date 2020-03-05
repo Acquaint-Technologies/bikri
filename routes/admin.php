04bf88312168266b7efeb547070c5480b643b5c2
Ferdous Anam <ferdous.anam@gmail.com>
2020-03-06 00:21:50 +0600

    set auth:admin middleware in Admin Routes

--- routes/admin.php
+++ routes/admin.php
@@ -19,25 +19,26 @@ Route::group(['prefix' => 'administrator'], function () {
         Route::post('logout', 'Auth\AdminLoginController@logout')->name('logout');
     });
 
-    Route::get('/', 'HomeController@index')->name('admin');
+    // Routes with auth:admin guard
+    Route::group(['middleware' => 'auth:admin'], function () {
+        Route::get('/', 'HomeController@index')->name('admin');
 
-    Route::get('/product-list', 'ProductController@adminProductList')->name('product-list');
+        Route::get('/product-list', 'ProductController@adminProductList')->name('product-list');
 
+        Route::get('/add-btype', 'BtypeController@addBtype')->name('add-btype');
+        Route::post('/save-btype', 'BtypeController@saveBtype')->name('save-btype');
+        Route::get('/view-btype', 'BtypeController@viewBtype')->name('view-btype');
+        Route::get('/btype/published/{id}', 'BtypeController@publishedBtype')->name('published-btype');
+        Route::get('/btype/unpublished/{id}', 'BtypeController@unpublishedBtype')->name('unpublished-btype');
+        Route::post('/btype/update-btype', 'BtypeController@updateBtype')->name('update-btype');
+        Route::get('/btype/delete-btype/{id}', 'BtypeController@deleteBtype')->name('delete-btype');
 
-    Route::get('/add-btype', 'BtypeController@addBtype')->name('add-btype');
-    Route::post('/save-btype', 'BtypeController@saveBtype')->name('save-btype');
-    Route::get('/view-btype', 'BtypeController@viewBtype')->name('view-btype');
-    Route::get('/btype/published/{id}', 'BtypeController@publishedBtype')->name('published-btype');
-    Route::get('/btype/unpublished/{id}', 'BtypeController@unpublishedBtype')->name('unpublished-btype');
-    Route::post('/btype/update-btype', 'BtypeController@updateBtype')->name('update-btype');
-    Route::get('/btype/delete-btype/{id}', 'BtypeController@deleteBtype')->name('delete-btype');
-
-
-    Route::get('/add-product-category', 'CategoryController@addProductCategory')->name('add-product-category');
-    Route::post('/save-product-category', 'CategoryController@saveProductCategory')->name('save-category');
-    Route::get('/view-product-category', 'CategoryController@viewProductCategory')->name('view-product-category');
-    Route::get('/product-category/published/{id}', 'CategoryController@publishedProductCategory')->name('published-category');
-    Route::get('/product-category/unpublished/{id}', 'CategoryController@unpublishedProductCategory')->name('unpublished-category');
-    Route::post('/update-product-category', 'CategoryController@updateProductCategory')->name('update-category');
-    Route::get('/delete-product-category/{id}', 'CategoryController@deleteProductCategory')->name('delete-category');
+        Route::get('/add-product-category', 'CategoryController@addProductCategory')->name('add-product-category');
+        Route::post('/save-product-category', 'CategoryController@saveProductCategory')->name('save-category');
+        Route::get('/view-product-category', 'CategoryController@viewProductCategory')->name('view-product-category');
+        Route::get('/product-category/published/{id}', 'CategoryController@publishedProductCategory')->name('published-category');
+        Route::get('/product-category/unpublished/{id}', 'CategoryController@unpublishedProductCategory')->name('unpublished-category');
+        Route::post('/update-product-category', 'CategoryController@updateProductCategory')->name('update-category');
+        Route::get('/delete-product-category/{id}', 'CategoryController@deleteProductCategory')->name('delete-category');
+    });
 });
